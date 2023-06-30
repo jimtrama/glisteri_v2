89873761829e54e4e58a6ceafa230520310dd19d
jim trama <dimeragon@gmail.com>
2023-07-01 00:08:46 +0300

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -6,24 +6,25 @@ function countUniqueIPs(filePath) {
     const lines = fileData.split('\n');
     const data = {};
     for (let i = 0; i < lines.length; i++) {
-      const line = lines[i].trim();
-      // Extract the IP address from each line
-      const date = line.split(' ')[1];
-      const ipAddress = line.split(' ')[4];
-      if(data.hasOwnProperty(date)){
-        let exists = fasle;
-        for ( let ip of data[date].value ){
-            if (ip == ipAddress){
-                exists = true;
-                break;
+        const line = lines[i].trim();
+        // Extract the IP address from each line
+        const date = line.split(' ')[1];
+        const ipAddress = line.split(' ')[4];
+        if(data.hasOwnProperty(date)){
+            let exists = false;
+            for ( let ip of data[date].values ){
+                if (ip == ipAddress){
+                    exists = true;
+                    break;
+                }
             }
+            if(!exists){
+                data[date].add(ipAddress);
+            }
+        
+        }else{
+            data[date] = new Set();
         }
-        if(!exists){
-            data[date].add(ipAddress);
-        }
-      }else{
-        data[date] = new Set();
-      }
     }
     for(let date in data){
         console.log(date + " : "+data[date].size());
